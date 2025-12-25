f58f2f3ea0d69f882339f3ea66603b00b754330c
Affan Dhankwala <affandhankwala@gmail.com>
2025-12-25 12:55:15 -0800

    Add readme and reorg

--- Research_Paper/Code/node.py
+++
@@ -1,6 +0,0 @@
-class Node:
-    def __init__(self, idx, vector, M=5):
-        self.idx = idx
-        self.vector = vector
-        self.neighbors = []  # list of neighbor node indices
-        self.M = M
